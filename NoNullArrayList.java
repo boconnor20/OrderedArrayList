991b3d5e14069363d11e19b8c8af976cde6de262
Brandon OConnor <boconnor20@stuy.edu>
2020-12-13 23:30:36 -0500

    added set and add()

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,8 +7,27 @@ public class NoNullArrayList<T> extends ArrayList<T>{
           throw new IllegalArgumentException("Element is null");
         }
         super.add(index,element);
-      }catch(IllegalArgumentException e){
+      }
+      catch(IllegalArgumentException e){
         throw new IllegalArgumentException("Element is null");
       }
     }
+    public boolean add(T element){
+      if (element == null) {
+        throw new IllegalArgumentException("Element is null");
+      }
+      super.add(element);
+      return true;
+    }
+    public T set (int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Element is null");
+    }
+    T old = super.get(index);
+    super.remove(index);
+    super.add(index, element);
+    return old;
+  }
+
+
 }
