6c65da6017a33491ecb461998ef9cc8732a844e8
Brandon OConnor <boconnor20@stuy.edu>
2020-12-14 00:06:07 -0500

    created a comparing helper method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,6 +1,16 @@
 import java.util.*;
 import java.io.*;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  private int helper(T value){
+    for(int i = 0; i < super.size(); i ++){
+      if(value.compareTo(super.get(i)) < 0){
+        int index = i;
+        return index;
+      }
+    }
+    return super.size();
+  }
+
   public boolean add(T element) {
       if (element == null) {
         throw new IllegalArgumentException("Your element is null");
@@ -8,21 +18,25 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super.add(element);//need to find index
     return true;
   }
+
   public void add(int index, T element) {
   if (element == null) {
     throw new IllegalArgumentException("Your element is null");
   }
     super.add(index, element);
   }
+
   public T set (int index, T element) {
   if (element == null) {
     throw new IllegalArgumentException("Element is null");
   }
+
   T old = super.get(index);
   super.remove(index);
   super.add(index, element);
   return old;
 }
+
 public OrderedArrayList() {
   super();
 }
