c004a9a7ea2a3b0a9d067ef6ef85b93edb0a5e81
Brandon OConnor <boconnor20@stuy.edu>
2020-12-14 00:00:41 -0500

    copied over methods and constructors

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,7 +5,32 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
       if (element == null) {
         throw new IllegalArgumentException("Your element is null");
       }
-    super.add(element);
+    super.add(element);//need to find index
     return true;
+  }
+  public void add(int index, T element) {
+  if (element == null) {
+    throw new IllegalArgumentException("Your element is null");
+  }
+    super.add(index, element);
+  }
+  public T set (int index, T element) {
+  if (element == null) {
+    throw new IllegalArgumentException("Element is null");
+  }
+  T old = super.get(index);
+  super.remove(index);
+  super.add(index, element);
+  return old;
+}
+public OrderedArrayList() {
+  super();
+}
+
+public OrderedArrayList(T startingCapacity) {
+  super();
+  if (startingCapacity == null) {
+      throw new IllegalArgumentException("The initial capacity cannot be null");
+  }
   }
 }
