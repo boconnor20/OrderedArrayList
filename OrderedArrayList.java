2d55eb04efca5ea33cbeffe9e3f6ff01beae0759
Brandon OConnor <boconnor20@stuy.edu>
2020-12-13 23:53:42 -0500

    downloaded Reng's testers and started working on OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,11 @@
-public class OrderedArrayList<T>{
-
+import java.util.*;
+import java.io.*;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public boolean add(T element) {
+      if (element == null) {
+        throw new IllegalArgumentException("Your element is null");
+      }
+    super.add(element);
+    return true;
+  }
 }
